bf391a62c6ced1840768b4d92ecbcd3a63a8a816
QuentinBisson <quentin@giantswarm.io>
2024-04-02 23:28:45 +0200

    add-mimir-heartbeat

---
+++ pkg/common/types.go
@@ -0,0 +1,8 @@
+package common
+
+type ManagementCluster struct {
+	// Name is the name of the management cluster.
+	Name string
+	// Pipeline is the pipeline name of the management cluster.
+	Pipeline string
+}
