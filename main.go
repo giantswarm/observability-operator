40102650c0e717cd4545a3a26dd00e2a1aa78b6b
Zirko <64951262+QuantumEnigmaa@users.noreply.github.com>
2024-10-14 11:37:06 +0200

    Update main.go
    Co-authored-by: Quentin Bisson <quentin@giantswarm.io>

--- main.go
+++ main.go
@@ -255,7 +255,7 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "Org")
+		setupLog.Error(err, "unable to create controller", "controller", "GrafanaOrganization")
 		os.Exit(1)
 	}
 	//+kubebuilder:scaffold:builder
