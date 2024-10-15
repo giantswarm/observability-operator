2456defdbaa26afee96a83fed204757b6c12cd6f
Quentin Bisson <quentin@giantswarm.io>
2024-10-15 14:17:03 +0200

    Update api/v1alpha1/grafanaorganization_types.go
    Co-authored-by: Théo Brigitte <theo.brigitte@gmail.com>

--- api/v1alpha1/grafanaorganization_types.go
+++ api/v1alpha1/grafanaorganization_types.go
@@ -36,6 +36,14 @@ type GrafanaOrganizationSpec struct {
 }
 
 // RBAC defines the RoleBasedAccessControl configuration for the Grafana organization.
+// Each fields represents the mapping to a Grafana role:
+//   Admin: full access to the Grafana organization
+//   Editor: edit resources in the Grafana organization
+//   Viewer: read only access to the Grafana organization
+// Each fields holds a list of string which represents values for a specific auth provider org attribute.
+// The org attribute is looked up using the `org_attribute_path` which is configured on your Grafana instance, see `https://<YOUR_GRAFANA_INSTANCE>/admin/settings`.
+// A user is granted a role when one of its org attribute value is contained within one of the role's values defined here.
+// More info on Grafana org attribute and role mapping at [Configure role mapping](https://grafana.com/docs/grafana/latest/setup-grafana/configure-security/configure-authentication/generic-oauth/#configure-role-mapping)
 type RBAC struct {
 	// Admins is a list of user organizations that have admin access to the grafanaorganization.
 	Admins []string `json:"admins"`
