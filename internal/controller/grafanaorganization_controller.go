cf6da30fc1762e0271eeca92fd54d8d8d3c5085f
QuentinBisson <quentin@giantswarm.io>
2024-11-04 15:23:54 +0100

    fix comment

--- internal/controller/grafanaorganization_controller.go
+++ internal/controller/grafanaorganization_controller.go
@@ -153,7 +153,7 @@ func (r GrafanaOrganizationReconciler) reconcileCreate(ctx context.Context, graf
 func (r GrafanaOrganizationReconciler) reconcileDelete(ctx context.Context, grafanaOrganization *v1alpha1.GrafanaOrganization) error {
 	logger := log.FromContext(ctx)
 
-	// We do not need to delete anything if there is no finalizer on the cluster
+	// We do not need to delete anything if there is no finalizer on the grafana organization
 	if controllerutil.ContainsFinalizer(grafanaOrganization, v1alpha1.GrafanaOrganizationFinalizer) {
 
 		// Delete organization in Grafana if it exists
