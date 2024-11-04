c1984a30bc8935220903abafa747985235907a0c
QuentinBisson <quentin@giantswarm.io>
2024-11-04 15:06:15 +0100

    clean up

--- tests/ats/test_olly_op.py
+++ tests/ats/test_olly_op.py
@@ -6,19 +6,12 @@ import pytest
 
 from pytest_helm_charts.clusters import Cluster
 from pytest_helm_charts.k8s.deployment import wait_for_deployments_to_run
-from pytest_helm_charts.giantswarm_app_platform.app import (
-    wait_for_apps_to_run,
-    AppFactoryFunc,
-    AppCR,
-)
-from pytest_helm_charts.utils import YamlDict
 
 logger = logging.getLogger(__name__)
 
 namespace_name = "monitoring"
 deployment_name = "observability-operator"
 timeout: int = 560
-
 
 @pytest.mark.smoke
 def test_api_working(kube_cluster: Cluster) -> None:
@@ -95,7 +88,6 @@ def pods(kube_cluster: Cluster, deployments: List[pykube.Deployment]) -> List[py
     )
 
     return pods
-
 
 # when we start the tests on circleci, we have to wait for pods to be available, hence
 # this additional delay and retries
