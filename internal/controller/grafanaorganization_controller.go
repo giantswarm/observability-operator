5e7740ef1a010dbbfe0d649ce5ea33c76595fb12
Quentin Bisson <quentin@giantswarm.io>
2024-11-07 14:37:46 +0100

    Update internal/controller/grafanaorganization_controller.go

--- internal/controller/grafanaorganization_controller.go
+++ internal/controller/grafanaorganization_controller.go
@@ -229,7 +229,6 @@ func (r GrafanaOrganizationReconciler) configureDatasources(ctx context.Context,
 		TenantID: grafanaOrganization.Name,
 	}
 
-	}
 
 	datasources, err := grafana.ConfigureDefaultDatasources(ctx, r.GrafanaAPI, organization)
 	if err != nil {
