cfccb2c820b13a974b299a56d9ce576a55f0c906
QuentinBisson <quentin@giantswarm.io>
2024-11-12 11:08:59 +0100

    edit roles for shared org

--- pkg/grafana/templating/templating.go
+++ pkg/grafana/templating/templating.go
@@ -32,8 +32,8 @@ func init() {
 func GenerateGrafanaConfiguration(organizations []v1alpha1.GrafanaOrganization) (string, error) {
 	var orgMappings []string
 	// TODO: We need to be admins to be able to see the private dashboards for now, remove the 2 GS groups once https://github.com/giantswarm/roadmap/issues/3696 is done.
-	orgMappings = append(orgMappings, buildOrgMapping("giantswarm-ad:giantswarm-admins", grafana.SharedOrg.Name, grafanaAdminRole))
-	orgMappings = append(orgMappings, buildOrgMapping("giantswarm-github:giantswarm:giantswarm-admins", grafana.SharedOrg.Name, grafanaAdminRole))
+	orgMappings = append(orgMappings, buildOrgMapping(grafana.SharedOrg.Name, "giantswarm-ad:giantswarm-admins", grafanaAdminRole))
+	orgMappings = append(orgMappings, buildOrgMapping(grafana.SharedOrg.Name, "giantswarm-github:giantswarm:giantswarm-admins", grafanaAdminRole))
 	orgMappings = append(orgMappings, fmt.Sprintf(`"*:%s:%s"`, grafana.SharedOrg.Name, grafanaEditorRole))
 	for _, organization := range organizations {
 		rbac := organization.Spec.RBAC
