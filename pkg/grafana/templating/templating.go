36e00c46b1aee2c7164d6adfa1fecd2a9165fbe2
Quentin Bisson <quentin@giantswarm.io>
2024-11-15 15:55:47 +0100

    Update pkg/grafana/templating/templating.go
    Co-authored-by: Théo Brigitte <theo.brigitte@gmail.com>

--- pkg/grafana/templating/templating.go
+++ pkg/grafana/templating/templating.go
@@ -32,8 +32,11 @@ func init() {
 func GenerateGrafanaConfiguration(organizations []v1alpha1.GrafanaOrganization) (string, error) {
 	var orgMappings []string
 	// TODO: We need to be admins to be able to see the private dashboards for now, remove the 2 GS groups once https://github.com/giantswarm/roadmap/issues/3696 is done.
+	// Grant Admin role to Giantswarm users logging in via ad (azure active directory).
 	orgMappings = append(orgMappings, buildOrgMapping(grafana.SharedOrg.Name, "giantswarm-ad:giantswarm-admins", grafanaAdminRole))
+	// Grant Admin role to Giantswarm users logging in via github.	
 	orgMappings = append(orgMappings, buildOrgMapping(grafana.SharedOrg.Name, "giantswarm-github:giantswarm:giantswarm-admins", grafanaAdminRole))
+	// Grant Editor role to every other users.
 	orgMappings = append(orgMappings, fmt.Sprintf(`"*:%s:%s"`, grafana.SharedOrg.Name, grafanaEditorRole))
 	for _, organization := range organizations {
 		rbac := organization.Spec.RBAC
