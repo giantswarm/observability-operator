d61b8b68b92520d5633b66bdb0502c4c449be991
Hervé Nicol <hervenicol@users.noreply.github.com>
2024-12-18 15:13:33 +0100

    add a local run tool for dev purposes (#202)
    * add a local run tool for dev purposes
    
    ---------
    
    Co-authored-by: Herve Nicol <12008875+hervenicol@users.noreply.github.com>

---
+++ hack/bin/run-local.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -euo pipefail
+
+# When developing the observability-operator, it is useful to run it locally against a real cluster.
+# This script sets up the environment and runs the operator locally.
+
+NAMESPACE="monitoring"
+declare -a OLLYOPARGS
+declare GRAFANAPORTFORWARDPID MIMIRPORTFORWARDPID
+
+
+# Define the arguments for the observability-operator
+function defineOLLYOPARGS {
+    # We take all args from the deployment, except the leader-elect one which we don't want for local development
+    mapfile -t OLLYOPARGS < <(kubectl -n "$NAMESPACE" get deployment observability-operator -ojson | jq -Mr '.spec.template.spec.containers[0].args[]' | grep -v "leader-elect")
+}
+
+# Populate environment variables
+function setEnvFromSecrets {
+  local specenv envname secretname secretkey envvalue
+
+  for specenv in $(kubectl get deployment -n "$NAMESPACE" observability-operator -ojson | jq -c -M '.spec.template.spec.containers[0].env[]') ; do
+    envname=$(echo "$specenv" | jq -r '.name')
+    secretname=$(echo "$specenv" | jq -r '.valueFrom.secretKeyRef.name')
+    secretkey=$(echo "$specenv" | jq -r '.valueFrom.secretKeyRef.key')
+    envvalue=$(kubectl get secret -n "$NAMESPACE" "$secretname" -ojson | jq -c -M -r '.data["'"$secretkey"'"]' | base64 -d)
+
+    echo "### setting $envname"
+    export "$envname"="$envvalue"
+
+  done
+}
+
+# Port-forward the Grafana service
+function grafanaPortForward {
+  kubectl port-forward -n "$NAMESPACE" svc/grafana 3000:80 &>/dev/null &
+  GRAFANAPORTFORWARDPID="$!"
+}
+
+# Stop the Grafana service port-forward
+function stopGrafanaPortForward {
+  kill "$GRAFANAPORTFORWARDPID" || true
+}
+
+# Port-forward the mimir service
+function mimirPortForward {
+  kubectl port-forward -n mimir svc/mimir-gateway 8180:80 &>/dev/null &
+  MIMIRPORTFORWARDPID="$!"
+}
+
+# Stop the Grafana service port-forward
+function stopMimirPortForward {
+  kill "$MIMIRPORTFORWARDPID" || true
+}
+
+# Pause the in-cluster operator
+function pauseInClusterOperator {
+    kubectl -n monitoring scale deployment observability-operator --replicas 0
+}
+
+# Resume the in-cluster operator
+function resumeInClusterOperator {
+    kubectl -n monitoring scale deployment observability-operator --replicas 1
+}
+
+# Cleanup function
+function cleanupAtExit {
+  stopGrafanaPortForward
+  stopMimirPortForward
+  resumeInClusterOperator
+}
+
+
+function main {
+
+  # make sure the script restores cluster at exit
+  trap cleanupAtExit SIGINT SIGQUIT SIGABRT SIGTERM EXIT
+
+  echo "### set env vars set"
+  setEnvFromSecrets
+  echo "### define ollyop args"
+  defineOLLYOPARGS
+  echo "### ollyorg args set"
+
+  echo "### starting port-forward"
+  grafanaPortForward
+  mimirPortForward
+
+  echo "### Pausing in-cluster operator"
+  pauseInClusterOperator
+
+  echo "### Running operator"
+  go run . "${OLLYOPARGS[@]}" -grafana-url http://localhost:3000 -monitoring-metrics-query-url http://localhost:8180
+
+  echo "### Cleanup"
+}
+
+main "$@"
