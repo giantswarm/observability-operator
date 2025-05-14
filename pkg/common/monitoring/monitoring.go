db6d3445f716853ade56c31950bf7bed48be5771
QuentinBisson <quentin@giantswarm.io>
2025-05-15 01:52:54 +0200

    small improvements

--- pkg/common/monitoring/monitoring.go
+++ pkg/common/monitoring/monitoring.go
@@ -51,7 +51,7 @@ const (
 	DefaultWriteTenant = "giantswarm"
 )
 
-var DefaultReadTenants = []string{"anonymous", "giantswarm"}
+var DefaultReadTenant = "giantswarm"
 
 func GetServicePriority(cluster *clusterv1.Cluster) string {
 	if servicePriority, ok := cluster.GetLabels()[servicePriorityLabel]; ok && servicePriority != "" {
