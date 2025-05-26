cb40d954cce7f0c7e11d403e0d79cb4c51c60ff7
Théo Brigitte <theo.brigitte@gmail.com>
2025-05-26 11:07:37 +0200

    Move create/delete logic for organizations and dashboards into grafana package (#395)

--- pkg/grafana/types.go
+++ pkg/grafana/types.go
@@ -3,6 +3,7 @@ package grafana
 import (
 	"strings"
 
+	"github.com/giantswarm/observability-operator/api/v1alpha1"
 	common "github.com/giantswarm/observability-operator/pkg/common/monitoring"
 )
 
@@ -13,6 +14,28 @@ type Organization struct {
 	Admins    []string
 	Editors   []string
 	Viewers   []string
+}
+
+func NewOrganization(grafanaOrganization *v1alpha1.GrafanaOrganization) Organization {
+	tenantIDs := make([]string, len(grafanaOrganization.Spec.Tenants))
+	for i, tenant := range grafanaOrganization.Spec.Tenants {
+		tenantIDs[i] = string(tenant)
+	}
+
+	orgID := grafanaOrganization.Status.OrgID
+	// Shared Org is the only exception to the rule as we know it's ID will always be 1
+	if grafanaOrganization.Spec.DisplayName == SharedOrg.Name {
+		orgID = SharedOrg.ID
+	}
+
+	return Organization{
+		ID:        orgID,
+		Name:      grafanaOrganization.Spec.DisplayName,
+		TenantIDs: tenantIDs,
+		Admins:    grafanaOrganization.Spec.RBAC.Admins,
+		Editors:   grafanaOrganization.Spec.RBAC.Editors,
+		Viewers:   grafanaOrganization.Spec.RBAC.Viewers,
+	}
 }
 
 type Datasource struct {
