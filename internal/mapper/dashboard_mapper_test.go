75ab33e4a11caec8c91a90d8cac817f4c5338c08
Quentin Bisson <quentin@giantswarm.io>
2025-06-19 14:46:32 +0200

    Update internal/mapper/dashboard_mapper_test.go

--- internal/mapper/dashboard_mapper_test.go
+++ internal/mapper/dashboard_mapper_test.go
@@ -2,6 +2,7 @@ package mapper
 
 import (
 	"testing"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/giantswarm/observability-operator/pkg/domain/dashboard"
