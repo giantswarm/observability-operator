a67bfce2d0be02feadcf0719b1915a7c09bf27e8
Théo Brigitte <theo.brigitte@gmail.com>
2025-11-11 15:08:30 +0100

    Make Ginkgo ignore integration tests

---
+++ tests/alertmanager-routes/phoenix/ginkgo-skip.go
@@ -0,0 +1,6 @@
+// Package main provides integration tests for phoenix alertmanager routes
+//
+// NOTE: This file is required to prevent Ginkgo test compilation failures.
+// Without a Go source file in this directory, Ginkgo will complain about
+// test compilation errors when attempting to discover and run tests.
+package main
