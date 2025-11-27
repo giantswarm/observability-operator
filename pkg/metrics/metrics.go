bc1a8ce9ee0f2923e0f35b963684c9bd5e023d0e
Theo Brigitte <theo.brigitte@gmail.com>
2025-11-27 11:36:25 +0100

    Add observability_operator_prometheus_target_info metrics

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -6,21 +6,29 @@ import (
 )
 
 var (
+	// MimirQueryErrors is a counter for tracking the number of errors while trying to query Mimir.
 	MimirQueryErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "observability_operator_mimir_head_series_query_errors_total",
 		Help: "Total number of reconciliations error",
 	}, nil)
 
-	// GrafanaOrganization metrics
+	// GrafanaOrganizationTenantInfo is a gauge for tracking tenant resources per organization.
 	GrafanaOrganizationTenantInfo = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "observability_operator_grafana_organization_tenants",
 		Help: "Information about tenant resources per organization",
 	}, []string{"name", "org_id"})
 
+	// GrafanaOrganizationInfo is a gauge for tracking information about GrafanaOrganization resources.
 	GrafanaOrganizationInfo = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "observability_operator_grafana_organization_info",
 		Help: "Information about GrafanaOrganization resources",
 	}, []string{"name", "display_name", "org_id", "status"}) // status: active, pending, error
+
+	// ObservabilityPrometheusTargetInfo is a gauge for tracking information about Alloy Prometheus targets.
+	ObservabilityPrometheusTargetInfo = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "observability_operator_prometheus_target_info",
+		Help: "Information about Alloy Prometheus targets",
+	}, []string{"app", "scrape_job"})
 )
 
 const (
@@ -34,5 +42,6 @@ func init() {
 		MimirQueryErrors,
 		GrafanaOrganizationTenantInfo,
 		GrafanaOrganizationInfo,
+		ObservabilityPrometheusTargetInfo,
 	)
 }
