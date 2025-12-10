6e30da85e3f1b5379820af1a4ade02d6610d416e
QuentinBisson <quentin@giantswarm.io>
2025-12-10 16:06:02 +0100

    use auth manager for logs and traces

--- pkg/config/logging.go
+++ pkg/config/logging.go
@@ -1,8 +1,39 @@
 package config
 
+import (
+	"strconv"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta1"
+)
+
+const LoggingLabel = "giantswarm.io/logging"
+
 // LoggingConfig represents the configuration used by the logging package.
 type LoggingConfig struct {
 	Enabled bool
+}
+
+// IsLoggingEnabled should be enabled when all conditions are met:
+//   - global logging flag is enabled
+//   - logging label is not set or is set to true on the cluster object
+func (c LoggingConfig) IsLoggingEnabled(cluster *clusterv1.Cluster) bool {
+	if !c.Enabled {
+		return false
+	}
+
+	// Check if label is set on the cluster object
+	labels := cluster.GetLabels()
+	loggingLabelValue, ok := labels[LoggingLabel]
+	if !ok {
+		// If it's not set, logging is enabled by default
+		return true
+	}
+
+	loggingEnabled, err := strconv.ParseBool(loggingLabelValue)
+	if err != nil {
+		return true
+	}
+	return loggingEnabled
 }
 
 // Validate validates the logging configuration
--- pkg/config/tracing.go
+++ pkg/config/tracing.go
@@ -1,8 +1,39 @@
 package config
 
+import (
+	"strconv"
+
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta1"
+)
+
+const TracingLabel = "giantswarm.io/tracing"
+
 // TracingConfig represents the configuration for tracing support in Grafana.
 type TracingConfig struct {
 	Enabled bool
+}
+
+// IsTracingEnabled should be enabled when all conditions are met:
+//   - global tracing flag is enabled
+//   - tracing label is not set or is set to true on the cluster object
+func (c TracingConfig) IsTracingEnabled(cluster *clusterv1.Cluster) bool {
+	if !c.Enabled {
+		return false
+	}
+
+	// Check if label is set on the cluster object
+	labels := cluster.GetLabels()
+	tracingLabelValue, ok := labels[TracingLabel]
+	if !ok {
+		// If it's not set, tracing is enabled by default
+		return true
+	}
+
+	tracingEnabled, err := strconv.ParseBool(tracingLabelValue)
+	if err != nil {
+		return true
+	}
+	return tracingEnabled
 }
 
 // Validate validates the tracing configuration
