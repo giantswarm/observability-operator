5222961db3faeedd359422347e1bcbe2e7e25664
QuentinBisson <quentin@giantswarm.io>
2026-01-13 10:43:20 +0100

    fix-tests-again

--- internal/webhook/v1/alertmanager_config_secret_webhook_test.go
+++ internal/webhook/v1/alertmanager_config_secret_webhook_test.go
@@ -27,6 +27,7 @@ import (
 
 	observabilityv1alpha1 "github.com/giantswarm/observability-operator/api/v1alpha1"
 	"github.com/giantswarm/observability-operator/pkg/alertmanager"
+	"github.com/giantswarm/observability-operator/pkg/common/tenancy"
 )
 
 var _ = Describe("Secret Webhook", func() {
@@ -65,7 +66,7 @@ receivers:
 		}
 		oldObj = &corev1.Secret{}
 		// Use the real client from the test environment
-		validator = AlertmanagerConfigSecretValidator{client: k8sClient}
+		validator = AlertmanagerConfigSecretValidator{client: k8sClient, tenantRepository: tenancy.NewKubernetesRepository(k8sClient)}
 		Expect(validator).NotTo(BeNil(), "Expected validator to be initialized")
 		Expect(oldObj).NotTo(BeNil(), "Expected oldObj to be initialized")
 		Expect(obj).NotTo(BeNil(), "Expected obj to be initialized")
