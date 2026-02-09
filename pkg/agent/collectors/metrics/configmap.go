f17402a0f0e577ea783ded5e52c0796e9ec8515d
QuentinBisson <quentin@giantswarm.io>
2026-02-09 14:03:56 +0100

    improve-metrics-tests

--- pkg/agent/collectors/metrics/configmap.go
+++ pkg/agent/collectors/metrics/configmap.go
@@ -62,6 +62,7 @@ func (a *Service) GenerateAlloyMonitoringConfigMapData(ctx context.Context, curr
 
 	// Compute the number of shards based on the number of series.
 	query := fmt.Sprintf(`sum(max_over_time((sum(prometheus_remote_write_wal_storage_active_series{cluster_id="%s", service="%s"})by(pod))[6h:1h]))`, cluster.Name, apps.AlloyMetricsAppName)
+
 	headSeries, err := querier.QueryTSDBHeadSeries(ctx, query, a.Config.Monitoring.MetricsQueryURL)
 	if err != nil {
 		logger.Error(err, "alloy-service - failed to query head series")
@@ -153,11 +154,11 @@ func (a *Service) generateAlloyConfig(ctx context.Context, cluster *clusterv1.Cl
 		AlloySecretName:      apps.AlloyMetricsAppName,
 		AlloySecretNamespace: apps.AlloyNamespace,
 
-		MimirRulerAPIURLKey:                   mimirRulerAPIURLKey,
-		MimirRemoteWriteAPIUsernameKey:        mimirRemoteWriteAPIUsernameKey,
-		MimirRemoteWriteAPIPasswordKey:        mimirRemoteWriteAPIPasswordKey,
-		MimirRemoteWriteAPIURLKey:             mimirRemoteWriteAPIURLKey,
-		MimirRemoteWriteAPINameKey:            mimirRemoteWriteAPINameKey,
+		MimirRulerAPIURLKey:                   common.MimirRulerAPIURLKey,
+		MimirRemoteWriteAPIUsernameKey:        common.MimirRemoteWriteAPIUsernameKey,
+		MimirRemoteWriteAPIPasswordKey:        common.MimirRemoteWriteAPIPasswordKey,
+		MimirRemoteWriteAPIURLKey:             common.MimirRemoteWriteAPIURLKey,
+		MimirRemoteWriteAPINameKey:            common.MimirRemoteWriteAPINameKey,
 		MimirRemoteWriteTimeout:               common.MimirRemoteWriteTimeout,
 		MimirRemoteWriteTLSInsecureSkipVerify: a.Config.Cluster.InsecureCA,
 
