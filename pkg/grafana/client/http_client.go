2d496703afd5b9091ed8123ebb3af26b67f44920
Quentin Bisson <quentin@giantswarm.io>
2026-02-24 10:44:45 +0100

    Apply suggestion from @QuentinBisson

--- pkg/grafana/client/http_client.go
+++ pkg/grafana/client/http_client.go
@@ -29,7 +29,7 @@ func (g *grafanaHTTPClient) OrgID() int64 {
 // NOTE: the upstream library's WithOrgID is not concurrent-safe until
 // https://github.com/grafana/grafana-openapi-client-go/pull/134 is merged.
 func (g *grafanaHTTPClient) WithOrgID(orgID int64) GrafanaClient {
-    g.api = g.api.WithOrgID(orgID)
+	g.api = g.api.WithOrgID(orgID)
 	return g
 }
 
