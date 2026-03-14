0d570400ecae2ecada615f3d2782bddc13d9a3a8
Quentin Bisson <quentin@giantswarm.io>
2026-03-14 19:18:35 +0100

    Update internal/controller/cluster_controller.go

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -380,7 +380,7 @@ func (r *ClusterMonitoringReconciler) reconcileAlloyServices(ctx context.Context
 	}
 
 	// alloy-event specific: Alloy events configuration - deployment that handles both kube event logs, traces and OTLP data - TODO rename alloy-events to alloy-cluster
-	otlpEnabled := r.Config.Monitoring.OTLPEnabled && r.Config.Monitoring.IsMonitoringEnabled(cluster) || (r.Config.Logging.OTLPEnabled && r.Config.Logging.IsLoggingEnabled(cluster))
+	otlpEnabled := (r.Config.Monitoring.OTLPEnabled && r.Config.Monitoring.IsMonitoringEnabled(cluster)) || (r.Config.Logging.OTLPEnabled && r.Config.Logging.IsLoggingEnabled(cluster))
 	if r.Config.Logging.IsLoggingEnabled(cluster) || r.Config.Tracing.IsTracingEnabled(cluster) || otlpEnabled {
 		// Create or update Alloy events configuration
 		err = r.AlloyEventsService.ReconcileCreate(ctx, cluster, observabilityBundleVersion)
